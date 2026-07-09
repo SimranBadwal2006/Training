86464123194aa50da6a89576558cb9afd2d89519
SimranBadwal2006 <simran26.simu@gmail.com>
2026-07-09 11:59:53 +0530

    DSA

--- DSA/day9/prog2.cpp
+++ DSA/day9/prog2.cpp
@@ -1,39 +1,48 @@
-#include<bits/stdc++.h>
+//find max and min by making  function
+#include<iostream>
 using namespace std;
-
-int find_max(int arr[], int n){
-    int max = arr[0];
-    for(int i = 0; i<n; i++){//traversing of array
-        if (arr[i] > max){
-            max = arr[i];
-        }
+int find_max(int arr[],int n){
+    int max=arr[0];
+    for(int i=0;i<n;i++){
+        if(arr[i]>max){
+    max=arr[i];
+}
     }
     return max;
+
+}
+int find_min(int arr[],int n){
+    int min=arr[0];
+    for(int i=0;i<n;i++){
+if(arr[i]<min){
+    min=arr[i];
+}
+    }
+    return min;
+
 }
 int main(){
-
     char str[26];
-    char ch ='A';
-    for(int i=0; i<26; i++){
-        str[i] = ch++;
-
+    char ch='A';
+    for(int i=0;i<26;i++){
+        str[i]=ch++;
     }
     //print
-    cout<<"A to Z:";
-    for(char ele : str){
-        cout<< ele <<" ";
+    cout<<"A to Z: ";
+    for(char ele:str){
+        cout<<ele<<" ";
     }
-    cout<<endl;
-    // int arr[5] = {[1]=2, [4]=3};//valid in c
     int n;
     cin>>n;
     int arr[n];
-
-    for(i=0; i<n; i++){
-        cin>>arr[i];
+    for(int i=0;i<n;i++){
+cin>>arr[i];
     }
+int max=find_max(arr,n);
+   cout<<"Maximum Element in Array :"<<max<<endl;
+        return 0;
 
-    int max = find_max(arr, n);
-    cout<<"Maximum: "<< max<<endl;
-    return 0;
+        int min=find_min(arr,n);
+   cout<<"Minimum Element in Array :"<<min<<endl;
+        return 0;
 }
